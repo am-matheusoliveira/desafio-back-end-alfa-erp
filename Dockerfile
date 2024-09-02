40cfe570ca3d34f49284deac591c4c1ddaf89efe
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2024-09-02 17:24:28 -0300

    1º commit neste repositório

--- Dockerfile
+++ Dockerfile
@@ -1,39 +1,22 @@
-# Use a imagem oficial do PHP como base
+# Imagem oficial do PHP
 FROM php:8.2.18-fpm
 
-# Instale dependências do sistema
+# Instale as dependências do sistema
 RUN apt-get update && apt-get install -y \
-    libpng-dev \
-    libjpeg-dev \
-    libfreetype6-dev \
-    libpq-dev \
-    libonig-dev \
-    locales \
-    zip \
-    unzip \
-    git \
-    curl \
-    && docker-php-ext-configure gd --with-freetype --with-jpeg \
-    && docker-php-ext-install gd pdo pdo_pgsql
+    libpq-dev \ 
+    && docker-php-ext-install pdo_pgsql
 
-# Instale Composer
+# Instale o Composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
-# Configure o diretório de trabalho
+# Configurando o diretório de trabalho
 WORKDIR /var/www/html
 
-# Copie os arquivos da aplicação
+# Copiando os arquivos da aplicação
 COPY . .
 
-# Instale as dependências do Laravel
-RUN composer install
-
-# Gere a chave da aplicação Laravel
-RUN php artisan key:generate
-
-# Configure permissões
+# Configurando as permissões
 RUN chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Exponha a porta 9000 e inicie o servidor PHP-FPM
-EXPOSE 9000
+# Iniciando o servidor PHP-FPM
 CMD ["php-fpm"]
--- app/Models/EstrategiaWms.php
+++ app/Models/EstrategiaWms.php
@@ -7,10 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Exception;
-use PDOException;
-use Symfony\Component\Console\Input\Input;
-
-use function Psy\debug;
 
 class EstrategiaWms extends Model
 {
@@ -19,74 +15,97 @@ class EstrategiaWms extends Model
     # BUSCA DOS REGISTROS
     public function select_register($cdEstrategia, $dsHora, $dsMinuto)
     {
+        // TRATAMENTO DE EXCEÇÃO PARA O PROCEDIMENTO
+        try{
 
-        // Verifica se a hora ou minutos tem exatamente 2 caracteres
-        $nv_hora    = (strlen($dsHora)   === 2) ? $dsHora   : str_pad($dsHora, 2,   '0', STR_PAD_LEFT);
-        $nv_minutos = (strlen($dsMinuto) === 2) ? $dsMinuto : str_pad($dsMinuto, 2, '0', STR_PAD_LEFT);
-        
-        // Concatenando a Hora e Minutos
-        $data_hora = $nv_hora.':'.$nv_minutos;
+            // Verifica se a hora ou minutos tem exatamente 2 caracteres
+            $nv_hora    = str_pad($dsHora, 2,   '0', STR_PAD_LEFT);
+            $nv_minutos = str_pad($dsMinuto, 2, '0', STR_PAD_LEFT);
+            
+            // Concatenando a Hora e Minutos
+            $data_hora = $nv_hora.':'.$nv_minutos;
 
-        // Buscando registro
-        $result_estrategia_wms = DB::table('tb_estrategia_wms_horario_prioridade')
-                                    ->where([
-                                        ['cd_estrategia_wms', '=', $cdEstrategia],
-                                        [DB::raw("TO_TIMESTAMP(ds_horario_inicio, 'HH24:MI')::TIME"), '<=', DB::raw("'".$data_hora."'"."::TIME")],
-                                        [DB::raw("TO_TIMESTAMP(ds_horario_final, 'HH24:MI')::TIME"), '>=',  DB::raw("'".$data_hora."'"."::TIME")]
-                                    
-                                    ])
-                                    ->select('nr_prioridade')->get();
+            // Buscando registro
+            $result_estrategia_wms = DB::table('tb_estrategia_wms_horario_prioridade')
+                                        ->where([
+                                            ['cd_estrategia_wms', '=', $cdEstrategia],
+                                            [DB::raw("TO_TIMESTAMP(ds_horario_inicio, 'HH24:MI')::TIME"), '<=', DB::raw("'".$data_hora."'"."::TIME")],
+                                            [DB::raw("TO_TIMESTAMP(ds_horario_final, 'HH24:MI')::TIME"), '>=',  DB::raw("'".$data_hora."'"."::TIME")]
+                                        
+                                        ])
+                                        ->select('nr_prioridade')
+                                        ->first();
 
-        // Se a busca acima vir vazia, retornar o padrão
-        if($result_estrategia_wms->count() === 0){
-            $result_estrategia_wms = DB::table('tb_estrategia_wms')
-                                        ->where('cd_estrategia_wms', '=', $cdEstrategia)
-                                        ->select('nr_prioridade')->get();
+            // Se a busca acima vir vazia, retornar o padrão
+            if(!$result_estrategia_wms){
+                $result_estrategia_wms = DB::table('tb_estrategia_wms')
+                                            ->where('cd_estrategia_wms', '=', $cdEstrategia)
+                                            ->select('nr_prioridade')
+                                            ->first();
+            }
+
+            // Novamente se a busca acima vir vazia, retornar uma mensagem, senão retornar o registro
+            if(!$result_estrategia_wms){
+
+                $response = response()->json(['nr_prioridade' => 'Nenhuma prioridade foi encontrada!'], 200);
+
+            }else{
+                
+                $response = response()->json(['nr_prioridade' => $result_estrategia_wms->nr_prioridade], 200);
+                
+            }
+
+        // Erros da aplicação
+        }catch(Exception $erro){        
+            
+            $response = response()->json(['exec_erro' => 'Erro, não foi possível realizar a busca do(s) registro(s), por favor tente novamente!'], 400);
+
+        }finally{
+
+            // Retornando resposta
+            return $response;
         }
-
-        // Novamente se a busca acima vir vazia, retornar 0 senão retornar o registro
-        if($result_estrategia_wms->count() === 0){
-            $response = response()->json( array('nr_prioridade' => 0), 200);
-        }else{
-            $response = response()->json( array('nr_prioridade' => $result_estrategia_wms[0]->nr_prioridade), 200);
-        }
-
-        // Retornando resposta
-        return $response;
     }   
 
     # INSERÇÃO DO REGISTRO
     public function insert_register(Request $request)
     {
-        // Inserindo na Tabela Pai
-        DB::table('tb_estrategia_wms')->insert([
-            'ds_estrategia_wms' => $request->input('dsEstrategia'),
-            'nr_prioridade'     => $request->input('nrPrioridade'),
-            'dt_modificado'     => date("Y-m-d H:i:s")
-        ]);
-        
-        // Ultimo ID Inserido
-        $ultimo_id = DB::getPdo()->lastInsertId();
 
-        // Inserindo na Tabela Filha
-        foreach($request->input('horarios') as $registro){
-            DB::table('tb_estrategia_wms_horario_prioridade')->insert([
-                'cd_estrategia_wms' => $ultimo_id,
-                'ds_horario_inicio' => $registro['dsHorarioInicio'],
-                'ds_horario_final'  => $registro['dsHorarioFinal'],
-                'nr_prioridade'     => $registro['nrPrioridade'],
-                'dt_modificado'     => date("Y-m-d H:i:s")                       
-            ]);
-        }
-        
-        // Se correu tudo certo retorna True
-        if($ultimo_id){
-            $response = response()->json( array('exec_sucesso' => true), 200);
-        }else{
-            $response = response()->json( array('exec_sucesso' => false), 200);
-        }
+        // TRATAMENTO DE EXCEÇÃO PARA O PROCEDIMENTO
+        try{
 
-        // Retornando resposta
-        return $response;        
+            DB::transaction(function () use ($request) {
+
+                // Inserindo na Tabela Pai
+                $ultimo_id = DB::table('tb_estrategia_wms')->insertGetId([
+                    'ds_estrategia_wms' => $request->input('dsEstrategia'),
+                    'nr_prioridade'     => $request->input('nrPrioridade'),
+                    'dt_modificado'     => date("Y-m-d H:i:s")
+                ], 'cd_estrategia_wms');                           
+
+                // Inserindo na Tabela Filha
+                foreach($request->input('horarios') as $registro){
+                    DB::table('tb_estrategia_wms_horario_prioridade')->insert([
+                        'cd_estrategia_wms' => $ultimo_id,
+                        'ds_horario_inicio' => $registro['dsHorarioInicio'],
+                        'ds_horario_final'  => $registro['dsHorarioFinal'],
+                        'nr_prioridade'     => $registro['nrPrioridade'],
+                        'dt_modificado'     => date("Y-m-d H:i:s")                       
+                    ]);
+                }
+            });
+
+            $response = response()->json( array('exec_sucesso' => 'Registro(s) salvo(s) com sucesso!'), 200);
+
+        // Erros da aplicação
+        }catch(Exception $erro){
+            
+            $response = response()->json( array('exec_erro' => 'Erro, não foi possível salvar o(s) registro(s)!'), 400);
+
+        }finally{
+
+            // Retornando resposta
+            return $response;
+        }
     }
 }
