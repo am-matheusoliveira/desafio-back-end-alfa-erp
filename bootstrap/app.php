88409927777e7fd1b53c667e18964bf31e3f2107
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2024-09-06 16:44:08 -0300

    1º commit neste repositório

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         
         $middleware->alias([            
-            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
             
         ]);
 
